1c440f9b42e8ffb2e2eefd8232cac3f04d080023
hoainam114 <44254752+hoainam114@users.noreply.github.com>
2018-10-25 15:20:48 +0700

    Add files via upload

---
+++ Lab 02/bt4.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+struct phanSo
+{
+	float tuSo;
+	float mauSo;
+};
+struct phanSo a,b;
+void cong(phanSo a,phanSo b)
+{
+	cout<<"Answer: "<<(a.tuSo*b.mauSo + a.mauSo*b.tuSo)<<"/"<<(a.mauSo*b.mauSo);
+	
+}
+void tru(phanSo a,phanSo b)
+{
+	cout<<"Answer: "<<(a.tuSo*b.mauSo - a.mauSo*b.tuSo)<<"/"<<(a.mauSo*b.mauSo);
+	
+}
+void nhan(phanSo a,phanSo b)
+{
+	cout<<"Answer: "<<(a.tuSo*b.tuSo)<<"/"<<(a.mauSo*b.mauSo);
+	
+}
+void chia(phanSo a,phanSo b)
+{
+	cout<<"Answer: "<<(a.tuSo*b.mauSo)<<"/"<<(a.mauSo*b.tuSo);
+	
+}
+int main()
+{
+	char op,DA;
+	do
+	{
+		cout<<"Nhap vao so a: "<<endl;
+		cin>>a.tuSo>>a.mauSo;
+		cout<<"Nhap vao so b: "<<endl;
+		cin>>b.tuSo>>b.mauSo;
+	}while(a.mauSo==0 || b.mauSo==0);
+	cout<<"So a = "<<a.tuSo<<"/"<<a.mauSo<<endl;
+	cout<<"So b = "<<b.tuSo<<"/"<<b.mauSo<<endl;
+	cout<<"Enter operator: ";
+	cin>>op;
+	if (op=='+')
+	{
+		cong(a,b);
+	}
+	if (op=='-')
+	{
+		tru(a,b);
+	}
+	if (op=='*')
+	{
+		nhan(a,b);
+	}
+	if (op=='/')
+	{
+		chia(a,b);
+	}
+	cout<<"Do another ?"<<endl;
+	cin>>DA;
+	while(DA=='y')
+	{
+		do
+	{
+		cout<<"Nhap vao so a: "<<endl;
+		cin>>a.tuSo>>a.mauSo;
+		cout<<"Nhap vao so b: "<<endl;
+		cin>>b.tuSo>>b.mauSo;
+	}while(a.mauSo==0 || b.mauSo==0);
+	cout<<"So a = "<<a.tuSo<<"/"<<a.mauSo<<endl;
+	cout<<"So b = "<<b.tuSo<<"/"<<b.mauSo<<endl;
+	cout<<"Enter operator: ";
+	cin>>op;
+	if (op=='+')
+	{
+		cong(a,b);
+	}
+	if (op=='-')
+	{
+		tru(a,b);
+	}
+	if (op=='*')
+	{
+		nhan(a,b);
+	}
+	if (op=='/')
+	{
+		chia(a,b);
+	}
+	cout<<"Do another ?"<<endl;
+	cin>>DA;
+	}
+	return 1;
+}
